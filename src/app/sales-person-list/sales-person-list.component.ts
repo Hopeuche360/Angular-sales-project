a581d863963014b6faebfe01ddda59094408fd35
Hopeuche360 <hopechijuka@gmail.com>
2021-11-08 20:49:57 +0100

    initial commit

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Hope", "Chijuka", "hopechijuka@gmail.com", 20000),
+    new SalesPerson("Ada", "Madu", "adamadu@gmail.com", 29000),
+    new SalesPerson("Anthony", "Idoko", "anthonyidoko@gmail.com", 30000),
+    new SalesPerson("Charlie", "Pexron", "charliepexron@gmail.com", 44000)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
